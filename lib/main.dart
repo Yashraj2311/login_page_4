36bf096954455a801234b92938231aef45291d89
Saswata Biswas <76907448+techie-coder@users.noreply.github.com>
2024-09-08 10:38:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_signup/screens/welcome_screen.dart';
 import 'package:login_signup/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
